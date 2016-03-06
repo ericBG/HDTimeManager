ac729ed2eb826a5cc20a87d05830a44607696f19
ericBG <ericrboidi@gmail.com>
2016-03-06 23:12:20 +0000

    The basics.

---
+++ HDTimeManager/HDTimeManager/HearthstoneTimeManagerPlugin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Controls;
+using Hearthstone_Deck_Tracker.Plugins;
+
+namespace HDTimeManager
+{
+    public class HearthstoneTimeManagerPlugin : IPlugin
+    {
+        public void OnLoad()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnUnload()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnButtonPress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name => "Hearthstone Time Manager";
+        public string Description => "A small plugin to manage your daily Hearthstone time.";
+        public string ButtonText => "Options";
+        public string Author => "ericBG";
+        public Version Version => new Version(1, 0, 0, 0);
+        public MenuItem MenuItem { get; }
+    }
+}
