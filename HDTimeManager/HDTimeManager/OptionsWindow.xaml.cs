050e501bdb11f4951b2927988f306f2350e21519
ericBG <ericrboidi@gmail.com>
2016-03-25 01:05:53 +0000

    Create a simple window to show the options for the time manager.

--- HDTimeManager/HDTimeManager/OptionsWindow.xaml.cs
+++ HDTimeManager/HDTimeManager/OptionsWindow.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using MahApps.Metro.Controls;
+﻿using System.Windows;
 
 namespace HDTimeManager
 {
@@ -27,6 +13,13 @@ namespace HDTimeManager
         {
             Config = c;
             InitializeComponent();
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            TimeRangeInfoWindow tsiw = new TimeRangeInfoWindow();
+            tsiw.Show();
+            if (tsiw.Result != null) Config.Ranges.Add(tsiw.Result);
         }
     }
 }
