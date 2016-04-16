893f84b76544e36c1996f2ab1d3dd91b66e2db85
ericBG <ericrboidi@gmail.com>
2016-04-16 19:36:06 +0100

    Removed files that were meant to be untracked... ONE TIME I use VSCode

--- HDTimeManager/HDTimeManager/build.cmd
+++
@@ -1,7 +0,0 @@
-if %1 == Debug (
-	start /wait taskkill /t /f /im HearthstoneDeckTracker.exe
-	if exist "C:\Users\EricAlejandro\Documents\GitHub\Hearthstone-Deck-Tracker\Hearthstone Deck Tracker\bin\Debug\Plugins\HDTimeManager.dll" (
-		del "C:\Users\EricAlejandro\Documents\GitHub\Hearthstone-Deck-Tracker\Hearthstone Deck Tracker\bin\Debug\Plugins\HDTimeManager.dll"
-	)
-	copy %2 "C:\Users\EricAlejandro\Documents\GitHub\Hearthstone-Deck-Tracker\Hearthstone Deck Tracker\bin\Debug\Plugins\HDTimeManager.dll"
-)
