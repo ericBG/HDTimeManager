6a75fbb406e300543281dde447c32803ebc7045a
ericBG <ericrboidi@gmail.com>
2016-04-16 23:51:11 +0100

    Clean up the final things for v1.0.0

--- HDTimeManager/HDTimeManager/HearthstoneTimeManagerPlugin.cs
+++ HDTimeManager/HDTimeManager/HearthstoneTimeManagerPlugin.cs
@@ -1,38 +1,28 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Windows.Controls;
-using Hearthstone_Deck_Tracker.Plugins;
+using System.Windows.Interop;
+using Hearthstone_Deck_Tracker;
 using Hearthstone_Deck_Tracker.API;
+using Hearthstone_Deck_Tracker.Plugins;
 using MahApps.Metro.Controls.Dialogs;
+using Core = Hearthstone_Deck_Tracker.API.Core;
 
 namespace HDTimeManager
 {
     public class HearthstoneTimeManagerPlugin : IPlugin
     {
-        private static Days Today => (Days)((int) Math.Pow(2, (int)DateTime.Today.DayOfWeek));
+        private static readonly MethodInfo FlashWindowInfo = typeof (User32).GetMethod("FlashWindow",
+            BindingFlags.NonPublic | BindingFlags.Static);
+
         internal ConfigInfo Config;
+        private static Days Today => (Days) ((int) Math.Pow(2, (int) DateTime.Today.DayOfWeek));
+
         public void OnLoad()
         {
             Config = ConfigInfo.Load();
             GameEvents.OnGameEnd.Add(CheckTime);
-        }
-
-        private void CheckTime()
-        {
-            TimeSpan timeSpent = Core.Game.CurrentGameStats.EndTime - Core.Game.CurrentGameStats.StartTime;
-            if (Config.DayLastUpdated != DateTime.Today)
-            {
-                Config.DayLastUpdated = DateTime.Today;
-                Config.TimeSpentToday = timeSpent;
-            }
-            else Config.TimeSpentToday += timeSpent;
-            Config.Save();
-            foreach (TimeRangeInfo info in Config.Ranges.Where(i => i.Active.HasFlag(Today)).Where(info => Config.TimeSpentToday > (info.Time - info.Range) && info.LastTriggered != DateTime.Today))
-            {
-                Core.MainWindow.ShowMessageAsync("WARNING", info.Message);
-                info.LastTriggered = DateTime.Today;
-            }
         }
 
         public void OnUnload()
@@ -51,5 +41,23 @@ namespace HDTimeManager
         public string Author => "ericBG";
         public Version Version => new Version(1, 0, 0, 0);
         public MenuItem MenuItem { get; }
+
+        private void CheckTime()
+        {
+            TimeSpan timeSpent = Core.Game.CurrentGameStats.EndTime - Core.Game.CurrentGameStats.StartTime;
+            if (Config.DayLastUpdated != DateTime.Today)
+            {
+                Config.DayLastUpdated = DateTime.Today;
+                Config.TimeSpentToday = timeSpent;
+            }
+            else Config.TimeSpentToday += timeSpent;
+            Config.Save();
+            foreach (var info in Config.Ranges.Where(i => i.Active.HasFlag(Today) && Config.TimeSpentToday > (i.Time - i.Range) && i.LastTriggered != DateTime.Today))
+            {
+                FlashWindowInfo.Invoke(null, new object[] {new WindowInteropHelper(Core.MainWindow).Handle, false});
+                Core.MainWindow.ShowMessageAsync("WARNING", info.Message);
+                info.LastTriggered = DateTime.Today;
+            }
+        }
     }
 }
